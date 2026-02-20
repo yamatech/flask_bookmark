d119d76a83235ff2eeb6032dae66414196a28e80
yamatech <akhr_ymn@me.com>
2026-02-20 21:26:43 +0900

    Formatted some of the code

--- models.py
+++ models.py
@@ -5,14 +5,14 @@ db = SQLAlchemy()
 
 
 class Bookmark(db.Model):
-    __tablename__ = 'bookmarks'
+    __tablename__ = "bookmarks"
 
-    id          = db.Column(db.Integer, primary_key=True)
-    title       = db.Column(db.String(200), nullable=False)
-    url         = db.Column(db.String(2048), nullable=False)
-    description = db.Column(db.Text, default='')
-    tag         = db.Column(db.String(100), default='')
-    created_at  = db.Column(db.DateTime, default=datetime.now)
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    url = db.Column(db.String(2048), nullable=False)
+    description = db.Column(db.Text, default="")
+    tag = db.Column(db.String(100), default="")
+    created_at = db.Column(db.DateTime, default=datetime.now)
 
     def __repr__(self):
-        return f'<Bookmark {self.title}>'
+        return f"<Bookmark {self.title}>"
